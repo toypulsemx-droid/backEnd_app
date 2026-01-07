5cf022bf104b7366e878fa96a54bcf7cc1dbe5a1
Francisco Melendez <toypulsemx@gmail.com>
2026-01-07 15:14:34 -0600

    ruta mongDB

--- Config/connectDB.js
+++ Config/connectDB.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI); // <- solo la URI
     console.log('✅ MongoDB conectado');
   } catch (error) {
     console.error('❌ Error al conectar MongoDB:', error.message);
